2536be6fe7bf9184a0b2a1314a2aa4798f094132
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-08-03 10:42:10 +0000

    l

---
+++ netflix/snapshots/snap_tags.sql
@@ -0,0 +1,21 @@
+{% snapshot snap_tags %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='row_key',
+        strategy='timestamp',
+        updated_at='tag_timestamp',
+        invalidate_hard_deletes=True
+    )
+}}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['user_id', 'movie_id', 'tag']) }} as row_key,
+    user_id,
+    movie_id,
+    tag,
+    cast(tag_timestamp as timestamp_ntz) as tag_timestamp
+from {{ ref('src_tags') }}
+
+{% endsnapshot %}
